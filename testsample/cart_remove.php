75ff4305f959e6ddab363ed6d0a07c1ab9b8f8ae
ZeruiCoding <zeruicode@gmail.com>
2025-11-27 12:01:00 +0800

    Delete testsample/cart_remove.php

--- testsample/cart_remove.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(isset($_GET['id'])) {
-    $id = $_GET['id'];
-    unset($_SESSION['cart'][$id]); // 从 Session 中删除
-}
-header("Location: cart.php"); //以此刷新页面
-exit();
-?>
